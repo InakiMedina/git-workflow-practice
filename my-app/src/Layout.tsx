bb5d3185207359cabfcd37f3a7b1ecaa6001cee7
InakiMedina <inaki.medina@gmail.com>
2026-08-31 20:43:23 -0600

    feature: added layout content

--- my-app/src/Layout.tsx
+++ my-app/src/Layout.tsx
@@ -1,7 +1,11 @@
 function Layout() {
 	return (
 		<>
-			<p>This is the layout</p>
+			<h1>Iñaki medina Mungía</h1>
+			<p>3326393009<br/>
+			Rinconada del Parque, Coto los manzanos<br/>
+			inaki.medina@iteso.mx<br/>
+			I’m a passionate science nerd. I love learning and studying for long hours. I’m currently studying software engineering. My main strenght and passion is to code algorithms with complex math. I have done tons of coding tourneys with problems just like that and likewise I’ve gone multiple times to math olimpiads.</p>
 		</>
 	)
 }
